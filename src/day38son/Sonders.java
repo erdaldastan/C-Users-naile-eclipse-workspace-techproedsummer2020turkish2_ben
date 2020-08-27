83a93ee1ec6f7cb16779bf965ff02150e48255d0
erdaldastan <erdaldastan69@gmail.com>
2020-08-27 03:53:18 +0300

    Second commit

--- src/day38son/Sonders.java
+++ src/day38son/Sonders.java
@@ -5,6 +5,7 @@ public class Sonders {
 	public static void main(String[] args) {
 		System.out.println("Java dersi bitti.");
 		System.out.println("Hayirli olsun.");
+		System.out.println("Sonunda commit yapabildim");
 
 	}
 
